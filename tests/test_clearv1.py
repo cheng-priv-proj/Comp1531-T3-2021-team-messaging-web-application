3f0472984b1806363d04755eaef986e2f3ecd550
Aslyui <alex.ljchen520@gmail.com>
2021-09-28 01:36:58 +1000

    fixed syntax

---
+++ tests/test_clearv1.py
@@ -0,0 +1,18 @@
+import pytest
+import ???
+
+from src.error import InputError
+from src.error import AccessError
+from data_store import data_store
+from data_store import Datastore
+
+# assumes that the data store is initially empty
+def test_clearv1_functionality():
+    empty_data_store = data_store.Datastore()
+    auth_user_id = auth_register_v1('test@gmail.com', 123, 'first', 'last')
+    clear_v1()
+    
+    assert empty_data_store == data_store
+
+def test_clearv1_returns_nothing():
+    assert clear_v1() == None
