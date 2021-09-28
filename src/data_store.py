e846b8624c13f32048c894a4d3237ee18c0c695c
very-smol-brain <76562119+very-smol-brain@users.noreply.github.com>
2021-09-29 01:52:26 +1000

    Updated datastore to latest version after it got deleted somehow

--- src/data_store.py
+++ src/data_store.py
@@ -1,35 +1,37 @@
-'''
-data_store.py
+# Dict 1:
+#   Key: Email
+#   Value: Dictionary {
+#          Password, 
+#          Auth_id}
+# Dict 2:
+#   Key: Auth_id
+#   Value: Dictionary { 
+#          u_id }
+# Dict 3:
+#   Key: Channel_id:
+#   Value: Dictionary {
+#          Channel Name
+#          Is Public/Private
+#          Messages
+#          owner_members
+#          all_members }
+# Dict 4: 
+#   (Matches ‘user’ type)
+#   Key: u_id:
+#   Value: Dictionary {
+#          u_id
+#          Email
+#          Name_first
+#          Name_last
+#          Handle_str }
 
-This contains a definition for a Datastore class which you should use to store your data.
-You don't need to understand how it works at this point, just how to use it :)
-
-The data_store variable is global, meaning that so long as you import it into any
-python file in src, you can access its contents.
-
-Example usage:
-
-    from data_store import data_store
-
-    store = data_store.get()
-    print(store) # Prints { 'names': ['Nick', 'Emily', 'Hayden', 'Rob'] }
-
-    names = store['names']
-
-    names.remove('Rob')
-    names.append('Jake')
-    names.sort()
-
-    print(store) # Prints { 'names': ['Emily', 'Hayden', 'Jake', 'Nick'] }
-    data_store.set(store)
-'''
 
 ## YOU SHOULD MODIFY THIS OBJECT BELOW
 initial_object = {
-    'login': {},
-    'auth_to_user': {},
-    'channel_id_to_channel_details': {},
-    'uid_to_user': {}
+    'email': {},
+    'auth_user_id': {},
+    'channel_id': {},
+    'u_id': {}
 }
 ## YOU SHOULD MODIFY THIS OBJECT ABOVE
 
@@ -52,7 +54,18 @@ class Datastore:
     def get_u_id_dict(self):
         return self.initial_object['u_id']
 
-    def update_value(dict_key, key, value):
+    def get_user_info_from_auth_id(self, auth_id):
+        u_id = self.initial_object['auth_user_id'].get(auth_id)
+        return self.initial_object['u_id'].get(u_id)
+
+    def check_user_is_member_of_channel(self, channel_id, u_id):
+        channel_details = self.get_channel_id_dict().get(channel_id)
+        if not any (member['u_id'] == u_id for member in channel_details['all_members']):
+            return False
+        
+        return True
+
+    def update_value(self, dict_key, key, value):
         self.initial_object[dict_key][key] = value
 
     def set(self, store):
