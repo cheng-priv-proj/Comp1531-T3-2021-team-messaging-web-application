2c8219bc1494237680fce5ca132ab5bd4be7d6b9
Aslyui <alex.ljchen520@gmail.com>
2021-09-28 22:57:01 +1000

    first draft of channel join

--- src/data_store.py
+++ src/data_store.py
@@ -1,35 +1,37 @@
-'''
-data_store.py
+# Dict 1:
+#   Key: Email
+#   Value: Dictionary {
+#          Password, 
+#          Auth_id}
+# Dict 2:
+#   Key: Auth_id
+#   Value: Dictionary { 
+#          u_id }
+# Dict 3:
+#   Key: Channel_id:
+#   Value: Dictionary {
+#          Channel Name
+#          Is Public/Private
+#          Messages
+#          owner_members
+#          all_members }
+# Dict 4: 
+#   (Matches ‘user’ type)
+#   Key: u_id:
+#   Value: Dictionary {
+#          u_id
+#          Email
+#          Name_first
+#          Name_last
+#          Handle_str }
 
-This contains a definition for a Datastore class which you should use to store your data.
-You don't need to understand how it works at this point, just how to use it :)
-
-The data_store variable is global, meaning that so long as you import it into any
-python file in src, you can access its contents.
-
-Example usage:
-
-    from data_store import data_store
-
-    store = data_store.get()
-    print(store) # Prints { 'names': ['Nick', 'Emily', 'Hayden', 'Rob'] }
-
-    names = store['names']
-
-    names.remove('Rob')
-    names.append('Jake')
-    names.sort()
-
-    print(store) # Prints { 'names': ['Emily', 'Hayden', 'Jake', 'Nick'] }
-    data_store.set(store)
-'''
 
 ## YOU SHOULD MODIFY THIS OBJECT BELOW
 initial_object = {
-    'login': {},
-    'auth_to_user': {},
-    'channel_id_to_channel_details': {},
-    'uid_to_user': {}
+    'email': {},
+    'auth_user_id': {},
+    'channel_id': {},
+    'u_id': {}
 }
 ## YOU SHOULD MODIFY THIS OBJECT ABOVE
 
