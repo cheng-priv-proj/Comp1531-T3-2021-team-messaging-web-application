f695b611952293b5a59052270924b2b0a9e92b02
Cheng Li <cheng.li9@student.unsw.edu.au>
2021-09-29 12:04:40 +1000

    implemented clear function

--- src/other.py
+++ src/other.py
@@ -2,5 +2,6 @@ from src.data_store import data_store
 
 def clear_v1():
     store = data_store.get()
-    store['users'] = []
+    for element in store:
+        store[element] = {}
     data_store.set(store)
