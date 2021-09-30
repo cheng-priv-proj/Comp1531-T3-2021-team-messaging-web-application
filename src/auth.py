23ead9f79028d6da9cb0d11ae3a335f82f1747d1
Brian Huang <brian.huang1@student.unsw.edu.au>
2021-10-01 01:37:08 +1000

    Merge auth_register with basic functionality working into Master

--- src/auth.py
+++ src/auth.py
@@ -1,12 +1,61 @@
 from src.data_store import data_store
 from src.error import InputError
+from src.other import handle_str_generation
+import re
 
 def auth_login_v1(email, password):
     return {
         'auth_user_id': 1,
     }
 
+
 def auth_register_v1(email, password, name_first, name_last):
+    # Updates dict 1, 2, 4 with new entries
+    # Generates u_id (negative), auth_id (positive), handle_str, checking for duplicate handles
+    
+    # checking for types
+    if type(email) != str:
+        raise TypeError
+    if type(password) != str:
+        raise TypeError
+    if type(name_first) != str:
+        raise TypeError
+    if type(name_last) != str:
+        raise TypeError
+
+    # input errors
+    if re.fullmatch(r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$', email):
+        pass
+    else:
+        raise InputError
+
+    
+    if len(password) < 6:
+        raise InputError
+    if len(name_first) < 1 or len(name_first) > 50:
+        raise InputError
+    if len(name_last) < 1 or len(name_last) > 50:
+        raise InputError
+    
+    #check if email is already being used
+    email_dict = data_store.get_login_from_email_dict()
+    if email in email_dict:
+        raise InputError
+
+    # generate u_id, auth_id and handle_str
+    u_id_dict = data_store.get_users_from_u_id_dict()
+    num_keys = len(u_id_dict)
+    auth_id = num_keys
+    u_id = -1 * auth_id
+
+    handle_str = handle_str_generation(name_first, name_last)
+
+    # insert new data into dicts
+    data_store.insert_user_info(u_id, email, name_first, name_last, handle_str)
+    data_store.insert_u_id(u_id, auth_id)
+    data_store.insert_login(email, password, auth_id)
+
+
     return {
-        'auth_user_id': 1,
+        'auth_user_id': auth_id
     }
