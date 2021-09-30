5c7c1c65e49bef343e63bc954ab27d3be078303a
Aslyui <alex.ljchen520@gmail.com>
2021-09-30 13:31:19 +1000

    fixed clear from iterating over keys to iterating over values

--- src/other.py
+++ src/other.py
@@ -2,6 +2,6 @@ from src.data_store import data_store
 
 def clear_v1():
     store = data_store.get()
-    for element in store:
+    for element in store.values():
         store[element] = {}
     data_store.set(store)
