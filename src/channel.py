98a9d37804a8d776cd8d3c881bcd5c4c9d845864
Teewin Xiao <teewin.xiao@student.unsw.edu.au>
2021-10-02 18:05:19 +1000

    fixed docstring for channel_invite

--- src/channel.py
+++ src/channel.py
@@ -72,7 +72,8 @@ Exceptions:
     TypeError   - occurs when auth_user_id, channel_id are not ints
     AccessError - occurs when auth_id is invalid
     InputError  - occurs when channel_id is invalid
-    InputError  - 
+    AccessError - occurs when channel_id is valid but the authorised user is
+                  not a member of the channel
 
 Return value:
     Returns nothing on success
