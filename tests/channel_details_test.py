41cdb3c162549cd5f698a7ae749f67e342864477
Cheng Li <cli130103@gmail.com>
2021-10-03 17:59:57 +1100

    Style review

--- tests/channel_details_test.py
+++ tests/channel_details_test.py
@@ -8,18 +8,14 @@ from src.auth import auth_register_v1
 from src.error import InputError
 from src.error import AccessError
 
-# parameters: auth_user_id (int), channel_id (int)
-# return type: name (string), is_public (boolean), owner_members & all_members (list of dicts, each contains types of user)
-
-
-# Returns only the value and not the dict
+# Extracts the auth_user_id from a given dictionary.
 @pytest.fixture
 def extract_user():
     def extract_user_id_function(auth_user_id_dict):
         return auth_user_id_dict['auth_user_id']
     return extract_user_id_function
 
-# Returns only the value and not the dict
+# Extracts the channel from a given dictionary.
 @pytest.fixture
 def extract_channel():
     def extract_channel_id_function(channel_id_dict):
@@ -38,8 +34,8 @@ def register():
     channel_id_dict = channels_create_v1(owner_user_id, 'test channel', True)
     return {**owner_id_dict, **channel_id_dict}
 
+# Test expecting AccessError when given a valid Auth_id that is not in the channel.
 def test_user_with_no_access_to_channel(clear, register, extract_channel, extract_user):
-    #access error if user not part of channel but id is valid
     auth_user_id_invalid = extract_user(auth_register_v1("invalid@email.com", "12345678", "testname", "testlname"))
     channel_1 = extract_channel(register)
 
@@ -49,21 +45,18 @@ def test_user_with_no_access_to_channel(clear, register, extract_channel, extrac
 def test__invalid_types(clear, register, extract_channel, extract_user):
     with pytest.raises(TypeError):
         channel_details_v1({}, {})
-    
+
 def test_invalid_channel_id(clear, register, extract_user):
-    #input error
     auth_user_id = extract_user(register)
 
     with pytest.raises(InputError):
         channel_details_v1(auth_user_id, 10000)
 
 def test_invalid_auth_id(clear, register, extract_channel):
-    #input error
     channel_id = extract_channel(register)
 
     with pytest.raises(AccessError):
         channel_details_v1(10000, channel_id)
-
 
 def test_returns_all_info(clear, register, extract_user, extract_channel):
     auth_user_id = extract_user(register)
