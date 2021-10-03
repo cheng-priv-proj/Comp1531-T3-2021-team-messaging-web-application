bb0c17b1249cbf1da1d016be4cc6b9c4976b44b2
Cheng Li <cli130103@gmail.com>
2021-10-03 18:28:45 +1100

    Style edits

--- tests/channel_messages_test.py
+++ tests/channel_messages_test.py
@@ -1,13 +1,17 @@
 import pytest
 
 from src.auth import auth_register_v1
+
 from src.channel import channel_messages_v1
+
 from src.channels import channels_create_v1
+
 from src.other import clear_v1
 
 from src.error import InputError
 from src.error import AccessError
 
+# Registers a user who is the owner of a created channel. 
 @pytest.fixture
 def clear_and_register():
     clear_v1()
@@ -32,6 +36,7 @@ def test_invalid_channel_id(clear_and_register):
     with pytest.raises(InputError):
         channel_messages_v1(auth_user_id, 123123123123, 0)
 
+# Test expecting InputError when given a negative starting index.
 def test_negative_start(clear_and_register):
     auth_user_id = clear_and_register[0]
     channel_id = clear_and_register[1]
@@ -44,12 +49,14 @@ def test_start_greater_than_messages(clear_and_register):
     with pytest.raises(InputError):
         channel_messages_v1(auth_user_id, channel_id, 10000)
 
+# Test that expects AccessError priority when both message index and auth id are invalid.
 def test_start_greater_than_messages_and_invalid_auth_id(clear_and_register):
     invalid_auth_user_id = 100
     channel_id = clear_and_register[1]
     with pytest.raises(AccessError):
         channel_messages_v1(invalid_auth_user_id, channel_id, 10000)
 
+# Test that expects AccessError priority when both channel and auth id are invalid.
 def test_invalid_channel_id_and_unauthorized_user(clear_and_register):
     with pytest.raises(AccessError):
         channel_messages_v1(123123123, 12312312345, 0)
