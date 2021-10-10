9d60d52302347d157df25155ece06fb51b10d7e9
Cheng Li <cli130103@gmail.com>
2021-10-10 23:47:53 +1100

    did a few more tests fixed some mistakes

--- tests/auth_login_v2_tests.py
+++ tests/auth_login_v2_tests.py
@@ -11,8 +11,7 @@ from src.error import InputError
 from src.other import clear_v2
 from src.other import clear_v1
 
-# clear_v2, auth_register_v2
-
+# clear_v2
 
 # Fixture to reset data store before every test
 @pytest.fixture
@@ -39,31 +38,32 @@ def extract_user_v2_token():
 # Fixture that registers a valid user.
 @pytest.fixture
 def auth_id_v2(clear_server):
-    return auth_register_v2("example@email.com", "potato", "john", "smith")
+    response = requests.post(config.url + 'auth/register/v2', data={
+        'email': 'example@email.com', 
+        'password': 'potato', 
+        'name_first': 'John', 
+        'name_last' : 'smith'
+        })
+    return response.json()
 
 
+def test_standard_v2(auth_id_v2, extract_user_v2):
 
-def test_standard_v2(clear, auth_id_v2, extract_user_v2, extract_user_v2_token):
-    user_dict = auth_login_v2("example@email.com", "potato")
-    auth_login_id = extract_user_v2(user_dict)
-    token = extract_user_v2_token(user_dict)
-    assert auth_login_id == 
+    requests.post(config.url + 'auth/login/v2', data={'email': 'example@email.com', 'password': 'potato'}).json()
+    dict_id_token = requests.get(config.url + 'auth/login/v2', params={'email': 'example@email.com', 'password': 'potato'}).json()
+    dict_id_token_register = (requests.get(config.url + 'auth/register/v2', params={'email': 'example@email.com', 'password': 'potato', 'name_first': 'John', 'name_last' : 'smith'})).json()
     
-    
-    assert isinstance(auth_login_id, int)
-    assert auth_login_id == au
+    assert extract_user_v2(dict_id_token) == extract_user_v2(dict_id_token_register), 'Valid case: Auth_id not successful'
 
-def test_incorrect_password_v2(clear, auth_id):
-    with pytest.raises(InputError):
-        auth_login_v1("example@email.com", "notpotato")
+def test_incorrect_password_v2(auth_id_v2):
+    resp = requests.post(config.url + 'auth/login/v2', data={'email': 'example@email.com', 'password': 'rongPASSWORD'})
+    assert(resp.status_code == 400)
+
 
 # Test that expects an input error when given a non-existant email.
-def test_email_does_not_exist_v2(clear, auth_id):
-    with pytest.raises(InputError):
-        auth_login_v1("invalid@gmail.com", "password")
-
-    with pytest.raises(InputError):
-        auth_login_v1("inv$alid@gmail.com", "password")
+def test_email_does_not_exist_v2(auth_id_v2):
+    resp = requests.post(config.url + 'auth/login/v2', data={'email': 'baaaaddexample@email.com', 'password': 'potato'})
+    assert(resp.status_code == 400)
 
 # Test that expects an input error when all the fields are invalid. 
 def test_all_incorrect_v2(clear, auth_id):
