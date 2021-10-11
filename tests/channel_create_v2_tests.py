f1e0ac4f679d14a5a1911e47829a2da212766831
Cheng Li <cli130103@gmail.com>
2021-10-11 17:13:21 +1100

    more basic test framework for channel create

--- tests/channel_create_v2_tests.py
+++ tests/channel_create_v2_tests.py
@@ -12,33 +12,9 @@ from src.auth import auth_register_v1
 
 from src.error import AccessError, InputError
 
-# Extracts the auth_user_id from a given dictionary.
-@pytest.fixture
-def extract_user():
-    def extract_user_id_function(auth_user_id_dict):
-        return auth_user_id_dict['auth_user_id']
-    return extract_user_id_function
-
-# Extracts the channel from a given dictionary.
-@pytest.fixture
-def extract_channel():
-    def extract_channel_id_function(channel_id_dict):
-        return channel_id_dict['channel_id']
-    return extract_channel_id_function
-
 @pytest.fixture
 def clear():
     clear_v1()
-
-# Automatically creates owner user id and channel id. Both are 1 by default.
-@pytest.fixture
-def register():
-    owner_id_dict = auth_register_v1('owner@test.com', 'password', 'owner', 'one')
-    owner_user_id = owner_id_dict['auth_user_id']
-    channel_id_dict = channels_create_v1(owner_user_id, 'test channel', True)
-    return {**owner_id_dict, **channel_id_dict}
-
-
 
 @pytest.fixture
 def get_valid_token(clear_server):
@@ -53,21 +29,21 @@ def get_valid_token(clear_server):
 
 
 # Tests that a public channel is created correctly.
-def test_public_channel(get_valid_token):
+def test_public_channel_v2(get_valid_token):
     channel_dict = requests.post(config.url + 'channels/create/v2', data={'token': get_valid_token, 'name': 'test channel', 'is_public': True}).json()
     extracted_channel_id = channel_dict['channel_id']
     details = requests.get(config.url + 'channel/details/v2', params={'token': get_valid_token, 'channel_id': extracted_channel_id}).json()
     assert details["is_public"] == True
 
 # Tests that a private channel is created correctly.
-def test_private_channel(get_valid_token):
+def test_private_channel_v2(get_valid_token):
     channel_dict = requests.post(config.url + 'channels/create/v2', data={'token': get_valid_token, 'name': 'test channel', 'is_public': False}).json()
     extracted_channel_id = channel_dict['channel_id']
     details = requests.get(config.url + 'channel/details/v2', params={'token': get_valid_token, 'channel_id': extracted_channel_id}).json()
     assert details["is_public"] == False
 
 # Tests that all generated channel id is unique
-def test_unique_channel_id(get_valid_token):
+def test_unique_channel_id_v2(get_valid_token):
     channel_dict1 = requests.post(config.url + 'channels/create/v2', data={'token': get_valid_token, 'name': 'test channel', 'is_public': False}).json()
 
     extracted_channel_id1 = channel_dict1['channel_id']
@@ -79,14 +55,24 @@ def test_unique_channel_id(get_valid_token):
     assert extracted_channel_id2 != extracted_channel_id1
 
 # Testing that the stream owner has correct permissions.
-def test_creator_joins_channel(clear, register, extract_user, extract_channel):
-    auth_user_id = extract_user(register)
-    channel_1 = extract_channel(register)
-    
-    details = channel_details_v1(auth_user_id, channel_1)
+def test_creator_joins_channel_v2(clear_server):
+    response = requests.post(config.url + 'auth/register/v2', data={
+        'email': 'owner@test.com', 
+        'password': 'potato', 
+        'name_first': 'John', 
+        'name_last' : 'smith'
+        })
+    return_dict = response.json()
+    token = return_dict['token']
+    auth_user_id = return_dict['auth_user_id']
+
+    channel_dict = requests.post(config.url + 'channels/create/v2', data={'token': token, 'name': 'test channel', 'is_public': False}).json()
+    extracted_channel_id = channel_dict['channel_id']
+    details = requests.get(config.url + 'channel/details/v2', params={'token': token, 'channel_id': extracted_channel_id}).json()
     assert details["all_members"][auth_user_id]["email"] == "owner@test.com"
 
-def test_becomes_owner(clear_server):
+
+def test_becomes_owner_v2(clear_server):
     response = requests.post(config.url + 'auth/register/v2', data={
         'email': 'owner@test.com', 
         'password': 'potato', 
@@ -102,17 +88,15 @@ def test_becomes_owner(clear_server):
     details = requests.get(config.url + 'channel/details/v2', params={'token': token, 'channel_id': extracted_channel_id}).json()
     assert details["owner_members"][auth_user_id]["email"] == "owner@test.com"
 
-def test_short_channel_name(get_valid_token):
-    auth_user_id = extract_user(register)
+def test_short_channel_name_v2(get_valid_token):
+    resp = requests.post(config.url + 'channels/create/v2', data={'token': get_valid_token, 'name': '', 'is_public': False}).json()
+    assert(resp.status_code == 400)
     
-    with pytest.raises(InputError):
-        channels_create_v1(auth_user_id, "", False)
+def test_long_channel_name_v2(get_valid_token):
+    resp = requests.post(config.url + 'channels/create/v2', data={'token': get_valid_token, 'name': 'reallylongname1234567eallylongname12345671234567', 'is_public': False}).json()
+    assert(resp.status_code == 400)
 
-def test_long_channel_name(clear, register, extract_user):
-    auth_user_id = extract_user(register)
-    
-    with pytest.raises(InputError):
-        channels_create_v1(auth_user_id, "reallylongname1234567eallylongname12345671234567", False)
+
 
 
 
