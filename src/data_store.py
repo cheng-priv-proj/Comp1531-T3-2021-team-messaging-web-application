1c899cd48242adf4254a7ad9fb179afbafaeb50f
Teewin Xiao <teewin.xiao@student.unsw.edu.au>
2021-10-12 16:58:01 +1100

    added token storage and related methods for modifying them to the datastore, as well as completely removing the use of the  deprecated channel messages method

--- src/data_store.py
+++ src/data_store.py
@@ -4,7 +4,12 @@ import json
 #   Key: email
 #   Value: Dictionary {
 #          password, 
-#          auth_id } 
+#          auth_user_id } 
+#
+# Tokens:
+#   Key: token (str)
+#   Value: u_id
+#
 # Channels:
 #   Key: channel_id:
 #   Value: Dictionary {
@@ -45,6 +50,7 @@ import json
 ## YOU SHOULD MODIFY THIS OBJECT BELOW
 initial_object = {
     'login' : {},
+    'token' : {},
     'channels' : {},
     'dms': {},
     'message_ids' : {},
@@ -66,6 +72,7 @@ class Datastore:
             json.dump(
             {
                 'login' : {},
+                'token' : {},
                 'channels' : {},
                 'dms': {},
                 'message_ids' : {},
@@ -96,6 +103,14 @@ class Datastore:
     def get_login_from_email(self, email):
         return self.get_logins_from_email_dict().get(email)
     
+    # tokens
+
+    def get_u_ids_from_token_dict(self):
+        return self.__store['token']
+
+    def get_u_id_from_token(self, token):
+        return self.get_u_ids_from_token_dict().get(token)
+
     # channels
 
     def get_channels_from_channel_id_dict(self):
@@ -115,21 +130,13 @@ class Datastore:
     def get_dm_creator_from_dm_id(self, dm_id):
         return self.get_dms_from_dm_id_dict().get(dm_id).get('creator')
 
-    # DEPRECATED, PLEASE CHANGE TO UPDATED FUNCTION
-    def get_messages_from_channel_id_dict(self):
-        return self.__store['messages']
-
-    # DEPRECATED, PLEASE CHANGE TO UPDATED FUNCTION    
-    def get_message_from_channel_id(self, channel_id):
-        return self.get_messages_from_channel_id_dict().get(channel_id)
-
     # messages
 
-    def get_channel_or_dm_id_from_message_id_dict(self):
+    def get_channels_or_dms_id_from_message_id_dict(self):
         return self.__store['message_ids']
 
     def get_channel_or_dm_id_from_message_id(self, message_id):
-        return self.get_channel_or_dm_id_from_message_id_dict().get(message_id)
+        return self.get_channels_or_dms_id_from_message_id_dict().get(message_id)
 
     def get_messages_from_channel_or_dm_id_dict(self):
         return self.__store['messages']
@@ -153,6 +160,11 @@ class Datastore:
 
 
     # Check functions
+
+    def is_token_valid(self, token):
+        if token in self.get_u_ids_from_token_dict():
+            return True
+        return False
 
     def is_user_member_of_channel(self, channel_id, u_id):
 
@@ -203,6 +215,10 @@ class Datastore:
         }
         self.update_json()
 
+    def insert_token(self, token, auth_user_id):
+        self.get_u_ids_from_token_dict()[token] = auth_user_id
+        self.update_json()
+
     def insert_user(self, u_id, email, name_first, name_last, handle_str):
         self.get_users_from_u_id_dict()[u_id] = {
             'u_id': u_id,
@@ -240,6 +256,10 @@ class Datastore:
         self.update_json()
 
     # Other
+
+    def invalidate_token(self, token):
+        tokens = self.get_u_ids_from_token_dict()
+        del tokens[token]
 
     def set(self, store):
         if not isinstance(store, dict):
