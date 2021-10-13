fd367f4fea7876ca67b679c1ddb7d2dff8a3c713
Zoe <zoe.collins@student.unsw.edu.au>
2021-10-14 01:35:01 +1100

    Fixing tests to work with http

--- tests/dm_create_v1_test.py
+++ tests/dm_create_v1_test.py
@@ -2,9 +2,9 @@ import pytest
 import requests
 import json
 import flask
+from src import config
 
 from src.other import clear_v1
-from src.error import InputError
 
 # Fixture to reset data store
 @pytest.fixture
@@ -12,52 +12,73 @@ def clear_server():
     #clear_v2()
     pass
 
+# Fixture to register someone and returns a dictionary of {token, auth_user_id}
 @pytest.fixture
-def get_valid_token(clear_server):
-    response = requests.post(config.url + 'auth/register/v2', data={
+def get_valid_token():
+    response = requests.post(config.url + 'auth/register/v2', json={
         'email': 'example@email.com', 
         'password': 'potato', 
         'name_first': 'John', 
         'name_last' : 'smith'
-        })
-    token = response.json()
-    return token['token']
+    })
+    return response.json()
+
+@pytest.fixture
+def get_valid_token_2():
+    response = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'owner@test.com', 
+        'password': 'spotato', 
+        'name_first': 'owner', 
+        'name_last' : 'one'
+    })
+    return response.json()
+
+@pytest.fixture
+def get_valid_token_3():
+    response = requests.post(config.url + 'auth/register/v2', json={
+            'email': 'eexample@email.com',
+            'password': 'spotatoo', 
+            'name_first': 'Johno',
+            'name_last': 'smith',
+    })
+    return response.json()
 
 
-def test_dm_create_v1_invalid_u_id(clear_server):
-    with pytest.raises(InputError):
-        invalid_auth_ids = ['a', 'b', '-1']
-        dm_create_v1(get_valid_token, invalid_auth_ids)
+def test_dm_create_v1_invalid_u_id(clear_server, get_valid_token):
+    invalid_auth_ids = ['a', 'b', '-1']
+    resp = requests.post(config.url + 'dm/create/v1', json={'token': get_valid_token['token'], 'u_ids': invalid_auth_ids}).json()
+    
+    assert resp.status_code == 400
 
 # assumption: dm_ids are generated based on how many already exist
-def test_dm_create_v1_returns_id(clear_server):
-    valid_auth_ids = ['1', '2', '4', '3']
-    dm_id = dm_create_v1(get_valid_token, valid_auth_ids)
-    assert dm_id == 1
+def test_dm_create_v1_returns_id(clear_server, get_valid_token, get_valid_token_2, get_valid_token_3):
+    auth_ids = [get_valid_token['auth_user_id'], get_valid_token_2['auth_user_id']]
+    resp = requests.post(config.url + 'dm/create/v1', json={'token': get_valid_token_3['token'], 'u_ids': auth_ids}).json()
 
-def test_dm_create_v1_returns_unique_ids(clear_server):
-    valid_auth_ids_1 = ['1', '2', '3', '4']
-    valid_auth_ids_2 = ['3', '4', '5', '6']
+    assert resp['dm_id'] == 1
+
+def test_dm_create_v1_returns_unique_ids(clear_server, get_valid_token, get_valid_token_2, get_valid_token_3):
+    user1 = get_valid_token['auth_user_id']
+    user2 = get_valid_token_2['auth_user_id']
+    user3 = get_valid_token_3['auth_user_id']
     
-    dm_id1 = dm_create_v1(get_valid_token, valid_auth_ids_1)
-    dm_id2 = dm_create_v1(get_valid_token, valid_auth_ids_2)
-    assert dm_id1 != dm_id2
+    resp1 = requests.post(config.url + 'dm/create/v1', json={'token': user1['token'], 'u_ids': [user2, user3]}).json()
+    resp2 = requests.post(config.url + 'dm/create/v1', json={'token': user3['token'], 'u_ids': [user1, user2]}).json()
 
-def test_dm_create_v1_name(clear_server):
-    user_1 = auth_register('email@test.com', 'password', 'firstname', 'lastname')
-    user_2 = auth_register('email2@test.com', 'password2', 'othername', 'otherlastname')
+    assert resp1['dm_id'] != resp2['dm_id']
 
-    auth_ids = [user1['auth_user_id'], user2['auth_user_id']]
-    dm_id = dm_create(token, auth_ids)
+def test_dm_create_v1_name(clear_server, get_valid_token, get_valid_token_2, get_valid_token_3):
+    auth_ids = [get_valid_token['auth_user_id'], get_valid_token_2['auth_user_id']]
 
-    dm_details = dm_details_v1(token, dm_id)
-    assert dm_details['name'] == ['firstnamelastname', 'othernameotherlastname']
+    resp = requests.post(config.url + 'dm/create/v1', json={'token': get_valid_token_3['token'], 'u_ids': auth_ids}).json()
+    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token_3, 'dm_id': resp['dm_id']}).json()
 
-def test_dm_create_v1_members(clear_server):
-    valid_auth_ids = ['1', '2', '3', '4']
-    token = get_valid_token()
-    dm_id = dm_create(token, valid_auth_ids)
+    assert resp_details['name'] == ['Johnsmith', 'ownerone']
 
-    # not sure what this will actually return for members
-    dm_details = dm_details_v1(token, dm_id)
-    assert dm_details['members'] == ''
+def test_dm_create_v1_members(clear_server, get_valid_token, get_valid_token_2, get_valid_token_3):
+    auth_ids = [get_valid_token['auth_user_id'], get_valid_token_2['auth_user_id']]
+    resp = requests.post(config.url + 'dm/create/v1', json={'token': get_valid_token_3['token'], 'u_ids': auth_ids}).json()
+    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token_3, 'dm_id': resp['dm_id']}).json()
+
+    #not sure what members looks like
+    assert resp_details['members'] == ''
