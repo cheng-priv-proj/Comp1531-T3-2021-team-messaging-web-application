6fd342e2d617de3daf50b6c90d484dffb410d6dd
Zoe <zoe.collins@student.unsw.edu.au>
2021-10-14 01:53:46 +1100

    Fixing tests to work with http

--- tests/dm_details_v1_test.py
+++ tests/dm_details_v1_test.py
@@ -2,9 +2,9 @@ import pytest
 import requests
 import json
 import flask
+from src import config
 
 from src.other import clear_v1
-from src.error import InputError
 
 # Fixture to reset data store
 @pytest.fixture
@@ -23,34 +23,36 @@ def get_valid_token(clear_server):
     token = response.json()
     return token['token']
 
+@pytest.fixture
+def get_valid_token_2():
+    response = requests.post(config.url + 'auth/register/v2', json={
+        'email': 'owner@test.com', 
+        'password': 'spotato', 
+        'name_first': 'owner', 
+        'name_last' : 'one'
+    })
+    return response.json()
+
+@pytest.fixture
+def get_dm_id(get_valid_token, get_valid_token_2):
+    resp = requests.post(config.url + 'dm/create/v1', json={'token': get_valid_token['token'], 'u_ids': [get_valid_token_2['auth_user_id']]}).json()
+    return resp['dm_id']
+
 
 def test_dm_details_v1_invalid_dm_id(clear_server):
-    with pytest.raises(InputError):
-        dm_details(get_valid_token, '-10')
-    
-def test_dm_details_v1_auth_user_not_member(clear_server):
-    user1 = auth_register('email@test.com', 'password', 'firstname', 'lastname')
-    user2 = auth_register('email2@test.com', 'password2', 'othername', 'otherlastname')
+    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token, 'dm_id': -1}).json()
+    assert resp_details.status_code == 400
 
-    dm_id = dm_create_v1(get_valid_token, [user1['auth_user_id'], user2['auth_user_id']])
-    with pytest.raises(AccessError):
-        dm_details(get_valid_token, dm_id)
+def test_dm_details_v1_auth_user_not_member(clear_server):
+    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token, 'dm_id': get_dm_id}).json()
+    assert resp_details.status_code == 403
 
 def test_dm_details_v1_returns_name(clear_server):
-    user1 = auth_register('email@test.com', 'password', 'firstname', 'lastname')
-    user2 = auth_register('email2@test.com', 'password2', 'othername', 'otherlastname')
-    token = get_valid_token()
-
-    dm_id = dm_create_v1(token, [user1['auth_user_id'], user2['auth_user_id']])
-    dm_details = dm_details_v1(token, dm_id)
-    assert dm_details['name'] == ['firstnamelastname', 'othernameotherlastname']
+    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token, 'dm_id': get_dm_id}).json()
+    assert resp_details['name'] == ['Johnsmith', 'ownerone']
 
 def test_dm_details_v1_returns_members(clear_server):
-    user1 = auth_register('email@test.com', 'password', 'firstname', 'lastname')
-    user2 = auth_register('email2@test.com', 'password2', 'othername', 'otherlastname')
-    token = get_valid_token()
+    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token, 'dm_id': get_dm_id}).json()
 
-    dm_id = dm_create_v1(token, [user1['auth_user_id'], user2['auth_user_id']])
-    dm_details = dm_details_v1(token, dm_id)
-    #not sure what members is
-    assert dm_details['members'] == ''
+    #not sure what members looks like
+    assert resp_details['members'] == ''
