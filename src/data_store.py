013f563d20dd4e5d03a228ce517f81afce72dd75
Brian Huang <brian.huang1@student.unsw.edu.au>
2021-10-13 21:39:02 +1100

    Channels to Master

--- src/data_store.py
+++ src/data_store.py
@@ -90,7 +90,6 @@ class Datastore:
             json.dump(self.__store, FILE)
 
     # Get Functions ############################################################
-
     def get(self):
         return self.__store
 
@@ -108,7 +107,9 @@ class Datastore:
         return self.__store['token']
 
     def get_u_id_from_token(self, token):
-        return self.get_u_ids_from_token_dict().get(token)
+        check_none = self.get_u_ids_from_token_dict().get(token)
+
+        return -1 if check_none == None else check_none
 
     # channels
 
