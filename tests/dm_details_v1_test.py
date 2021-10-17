0c8c68664804988bfd0a98cf788f8286e6ff866c
Zoe <zoe.collins@student.unsw.edu.au>
2021-10-17 13:03:42 +1100

    Modifying tests o work, fixing style

--- tests/dm_details_v1_test.py
+++ tests/dm_details_v1_test.py
@@ -32,27 +32,97 @@ def get_valid_token_2():
         'name_last' : 'one'
     })
     return response.json()
+    
 
-@pytest.fixture
-def get_dm_id(get_valid_token, get_valid_token_2):
-    resp = requests.post(config.url + 'dm/create/v1', json={'token': get_valid_token['token'], 'u_ids': [get_valid_token_2['auth_user_id']]}).json()
-    return resp['dm_id']
+def test_invalid_token(clear_server, get_valid_token, get_valid_token_2):
+    user1 = get_valid_token
+    user2 = get_valid_token_2
+    invalid_token = '-10000'
 
+    dm_id = requests.post(config.url + 'dm/create/v1', json={
+        'token': user1['token'], 
+        'u_ids': [user2['auth_user_id']]
+    }).json()
 
-def test_dm_details_v1_invalid_dm_id(clear_server):
-    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token, 'dm_id': -1}).json()
-    assert resp_details.status_code == 400
-
-def test_dm_details_v1_auth_user_not_member(clear_server):
-    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token, 'dm_id': get_dm_id}).json()
+    resp_details = requests.get(config.url + 'dm/details/v1', json={
+        'token': invalid_token, 
+        'dm_id': dm_id['dm_id']
+        })
     assert resp_details.status_code == 403
 
-def test_dm_details_v1_returns_name(clear_server):
-    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token, 'dm_id': get_dm_id}).json()
-    assert resp_details['name'] == ['Johnsmith', 'ownerone']
+def test_dm_details_v1_invalid_dm_id(clear_server, get_valid_token):
+    resp_details = requests.get(config.url + 'dm/details/v1', json={
+        'token': get_valid_token, 
+        'dm_id': -1
+        })
+    assert resp_details.status_code == 400
 
-def test_dm_details_v1_returns_members(clear_server):
-    resp_details = requests.get(config.url + 'dm/details/v1', json={'token': get_valid_token, 'dm_id': get_dm_id}).json()
+def test_dm_details_v1_auth_user_not_member(clear_server, get_valid_token, get_valid_token_2):
+    user1 = get_valid_token
+    user2 = get_valid_token_2
+    
+    dm_id = requests.post(config.url + 'dm/create/v1', json={
+        'token': user1['token'], 
+        'u_ids': [user2['auth_user_id']]
+        }).json()
 
-    #not sure what members looks like
-    assert resp_details['members'] == ''
+    resp_details = requests.get(config.url + 'dm/details/v1', json={
+        'token': user1, 
+        'dm_id': dm_id['dm_id']
+        })
+    assert resp_details.status_code == 403
+
+def test_dm_details_v1_invalid_auth_user_and_dm_id(clear_server, get_valid_token):
+    resp_details = requests.get(config.url + 'dm/details/v1', json={
+        'token': get_valid_token, 
+        'dm_id': -1
+        })
+    # should return access error as priority
+    assert resp_details.status_code == 403
+
+def test_dm_details_v1_returns_name(clear_server, get_valid_token, get_valid_token_2):
+    user1 = get_valid_token
+    user2 = get_valid_token_2
+
+    dm_id = requests.post(config.url + 'dm/create/v1', json={
+        'token': user1['token'], 
+        'u_ids': [user2['auth_user_id']]
+        }).json()
+
+    resp_details = requests.get(config.url + 'dm/details/v1', json={
+        'token': user1, 
+        'dm_id': dm_id['dm_id']
+        }).json()
+
+    assert resp_details['name'] == 'Johnsmith, ownerone'
+
+def test_dm_details_v1_returns_members(clear_server, get_valid_token, get_valid_token_2):
+    user1 = get_valid_token
+    user2 = get_valid_token_2
+
+    dm_id = requests.post(config.url + 'dm/create/v1', json={
+        'token': user1['token'], 
+        'u_ids': [user2['auth_user_id']]
+        }).json()
+
+    resp_details = requests.get(config.url + 'dm/details/v1', json={
+        'token': user1, 
+        'dm_id': dm_id['dm_id']
+        }).json()
+
+    assert resp_details['members'] == [
+        {
+            'u_id': 1,
+            'Email': 'example@email.com',
+            'Name_first': 'John',
+            'Name_last': 'smith',
+            'Handle_str': 'Johnsmith'
+        },
+        {
+            'u_id': 2,
+            'Email': 'owner@test.com',
+            'Name_first': 'owner',
+            'Name_last': 'one',
+            'Handle_str': 'ownerone'
+        }
+    ]
