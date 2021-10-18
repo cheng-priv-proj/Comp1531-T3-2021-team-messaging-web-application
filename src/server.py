f72e7d02e9cd3858099245b781acdfb197f4d6a6
very-smol-brain <76562119+very-smol-brain@users.noreply.github.com>
2021-10-18 20:31:37 +1100

    Fixed merge

--- src/server.py
+++ src/server.py
@@ -6,17 +6,10 @@ from flask import Flask, request
 from flask_cors import CORS
 from src import config
 
-from src.channel import channel_join_v1, channel_details_v1
-from src.channels import channels_listall_v1, channels_list_v1
-
+from src.channels import channels_listall_v1, channels_list_v1, channels_create_v1
 from src.auth import auth_login_v1, auth_register_v1, auth_logout_v1
-from src.channels import channels_create_v1, channels_list_v1, channels_listall_v1
-
-from src.channel import channel_invite_v1, channel_messages_v1, channel_details_v1
 from src.dm import dm_create_v1, dm_details_v1, dm_remove_v1, dm_details_v1, dm_create_v1
-
 from src.channel import channel_invite_v1, channel_messages_v1, channel_details_v1, channel_leave_v1, channel_addowner_v1
-
 from src.user import user_profile_v1
 
 from src.data_store import data_store
