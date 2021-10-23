db8a924cd1f1609fbbddc19bc5a4ca1418ab6966
Cheng Li <cli130103@gmail.com>
2021-10-23 21:41:14 +1100

    style for  dm_messages_test

--- tests/dm_messages_test.py
+++ tests/dm_messages_test.py
@@ -51,6 +51,12 @@ def dm_factory():
     return create_dm
 
 def test_no_messages(register, dm_factory):
+    '''
+    Test case where there are no messages.
+
+    Expects: 
+        Correct output from dm/messages.
+    '''
 
     dm_id = dm_factory(register[0]['token'], [])
 
@@ -67,6 +73,13 @@ def test_no_messages(register, dm_factory):
     }
 
 def test_invalid_token_valid_dm_id(register, dm_factory):
+    '''
+    Test expecting access error when token and dm_id is invalid.
+
+    Expects: 
+        AccessError (403 error)
+    '''
+
     dm_id = dm_factory(register[0]['token'], [])
 
     assert requests.get(url + 'dm/messages/v1', params = {
@@ -76,6 +89,12 @@ def test_invalid_token_valid_dm_id(register, dm_factory):
     }).status_code == 403
 
 def test_invalid_dm_id(register):
+    '''
+    Test expecting input error when dm_id is invalid.
+
+    Expects: 
+        InputError (400 error)
+    '''
 
     assert requests.get(url + 'dm/messages/v1', params = {
         'token': register[0]['token'],
@@ -84,6 +103,13 @@ def test_invalid_dm_id(register):
     }).status_code == 400
 
 def test_invalid_start(register, dm_factory):
+    '''
+    Test expecting input error when start is invalid.
+
+    Expects: 
+        InputError (400 error)
+    '''
+
     dm_id = dm_factory(register[0]['token'], [])
 
     assert requests.get(url + 'dm/messages/v1', params = {
@@ -91,9 +117,15 @@ def test_invalid_start(register, dm_factory):
         'dm_id': dm_id,
         'start': 50
     }).status_code == 400
-    pass
 
 def test_negative_start(register, dm_factory):
+    '''
+    Test expecting input error when start is invalid.
+
+    Expects: 
+        InputError (400 error)
+    '''
+
     dm_id = dm_factory(register[0]['token'], [])
 
     assert requests.get(url + 'dm/messages/v1', params = {
@@ -104,6 +136,13 @@ def test_negative_start(register, dm_factory):
     pass
 
 def test_invalid_dm_and_token(clear):
+    '''
+    Test expecting Acccess error when token and dm is invalid.
+
+    Expects: 
+        AccessError (403 error)
+    '''
+
     assert requests.get(url + 'dm/messages/v1', params = {
         'token': 'no token has been registered',
         'dm_id': 1,
