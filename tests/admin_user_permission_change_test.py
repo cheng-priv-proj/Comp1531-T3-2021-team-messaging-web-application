9e12a4bd1a55c9a3e582ea7dea270c17c8bc3508
Cheng Li <cli130103@gmail.com>
2021-10-23 20:08:29 +1100

    style changes for  admin_user_perm_change_test

--- tests/admin_user_permission_change_test.py
+++ tests/admin_user_permission_change_test.py
@@ -9,9 +9,13 @@ import requests
 def clear():
     requests.delete(url + "clear/v1")
 
-# Generates the first user
+
 @pytest.fixture
 def first_register():
+    '''
+    Fixture for generating the first user
+    '''
+
     user_details = {
         'email': 'globalowner@test.com',
         'password': 'password', 
@@ -33,9 +37,12 @@ def first_register():
     
     return {'token': token, 'auth_user_id': auth_user_id, 'channel_id': int(channel_id)}
 
-# Creates a user using the given details and returns the channel_id
 @pytest.fixture 
 def second_register():
+    '''
+    Creates a user using the given details and returns the channel_id
+    '''
+
     user_details = {
         'email': 'globalowner2@test.com',
         'password': 'password', 
@@ -57,8 +64,13 @@ def second_register():
     
     return {'token': token, 'auth_user_id': auth_user_id, 'channel_id': int(channel_id)}
 
-# Test input u_id does not refer to valid
 def test_invalid_u_id(clear, first_register):
+    '''
+    Test input u_id does not refer to valid
+
+    Expects:
+        InputError (400 error) 
+    '''
     token = first_register.get('token')
     invalid_u_id = -10000
     permission_id = 1
@@ -69,8 +81,14 @@ def test_invalid_u_id(clear, first_register):
         'permission_id': permission_id
     }).status_code == 400
 
-# Test input permission id is invalid
 def test_invalid_permission_id(clear, first_register, second_register):
+    '''
+    Test input permission id is invalid
+
+    Expects: 
+        InputError (400 error) 
+    '''
+
     token = first_register.get('token')
     u_id = second_register.get('auth_user_id')
     invalid_permission_id = 3
@@ -81,8 +99,14 @@ def test_invalid_permission_id(clear, first_register, second_register):
         'permission_id': invalid_permission_id
     }).status_code == 400
 
-# Test u_id is the only global owner and demote to user
 def test_demote_only_owner(clear, first_register):
+    '''
+    Test u_id is the only global owner and demote to user
+
+    Expects: 
+        InputError (400 error) 
+    '''
+
     token = first_register.get('token')
     u_id = first_register.get('auth_user_id')
     permission_id = 2
@@ -93,8 +117,15 @@ def test_demote_only_owner(clear, first_register):
         'permission_id': permission_id
     }).status_code == 400
 
-# Test successful perms change
 def test_success_perms_change(clear, first_register, second_register):
+    '''
+    Test successful perms change
+
+    Expects: 
+        Success (status code 200)
+        InputError (400 error) 
+    '''
+
     first_token = first_register.get('token')
     first_u_id = first_register.get('auth_user_id')
 
@@ -125,23 +156,33 @@ def test_success_perms_change(clear, first_register, second_register):
         'permission_id': become_member
     }).status_code == 400
 
-# Access if not global owner
 def test_no_perms(clear, first_register, second_register):
+    '''
+    Test expecting error if requestee does not have the correct perms.
+
+    Expects: 
+        AccessError (403 error) 
+    '''
+
     first_u_id = first_register.get('auth_user_id')
 
     second_token = second_register.get('token')
 
     become_member = 2
 
-    # Makes first user member
     assert requests.post(url + 'admin/userpermission/change/v1', json = {
         'token': second_token, 
         'u_id': first_u_id,
         'permission_id': become_member
     }).status_code == 403
 
-# Access if not valid token
 def test_not_valid_token(clear, first_register, second_register):
+    '''
+    Test that expects an error when given a fake/invalid token
+
+    Expects: 
+        AccessError (403 error) 
+    '''
     invalid_token = '-10000'
     u_id = second_register.get('auth_user_id')
     permission_id = 1
