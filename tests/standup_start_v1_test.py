a780e07ab3bf6254cc4661951c1bfeaac9e20b66
Aslyui <alex.ljchen520@gmail.com>
2021-11-02 01:06:06 +1100

    fixing pylint

--- tests/standup_start_v1_test.py
+++ tests/standup_start_v1_test.py
@@ -1,10 +1,11 @@
 import pytest
 import requests
 from datetime import datetime
+from src.config import url
 
 @pytest.fixture
 def clear_server():
-    requests.delete(config.url + "clear/v1")
+    requests.delete(url + "clear/v1")
 
 # Extracts the token from a given dictionary.
 @pytest.fixture
@@ -51,56 +52,73 @@ def register_channel():
         return channel_id
     return register_channel_function
 
-def test_create_standup_basic_functionality(clear, register_channel, register_user, create_standup):
+def test_create_standup_basic_functionality(clear, register_channel, register_user, create_standup, extract_token):
     owner_info = register_user('owner@gmail.com', 'owner', 'one')
-    channel_id = register_channel(extract_token(owner_info), 'channel1', True).json()
-    standup_info = create_standup(extract_token(owner_info), channel_id, 1000).json()
+    owner_token = extract_token(owner_info)
+
+    channel_id = register_channel(owner_token, 'channel1', True).json()
+    standup_info = create_standup(owner_token, channel_id, 1000).json()
+
+    now = datetime.now().timestamp()        
     assert standup_info['time_finish'] == pytest.approx(now + 1000, rel=2)
 
-def test_create_standup_no_second_standup(clear, register_channel, register_user, create_standup):
+def test_create_standup_no_second_standup(clear, register_channel, register_user, create_standup, extract_token):
     owner_info = register_user('owner@gmail.com', 'owner', 'one')
-    channel_id = register_channel(extract_token(owner_info), 'channel1', True).json()
-    create_standup(extract_token(owner_info), channel_id, 1000).json()
-    error = create_standup(extract_token(owner_info), channel_id, 1000).status_code
+    owner_token = extract_token(owner_info)
+
+    channel_id = register_channel(owner_token, 'channel1', True).json()
+    create_standup(owner_token, channel_id, 1000).json()
+    error = create_standup(owner_token, channel_id, 1000).status_code
 
     assert error == 400
 
-def test_create_standup_invalid_channel_id(clear, register_channel, register_user, create_standup):
+def test_create_standup_invalid_channel_id(clear, register_user, create_standup, extract_token):
     owner_info = register_user('owner@gmail.com', 'owner', 'one')
-    error = create_standup(extract_token(owner_info), 12312, 1000).status_code
+    owner_token = extract_token(owner_info)
+
+    error = create_standup(owner_token, 12312, 1000).status_code
 
     assert error == 400
 
-def test_create_standup_negative_length(clear, register_channel, register_user, create_standup):
+def test_create_standup_negative_length(clear, register_channel, register_user, create_standup, extract_token):
     owner_info = register_user('owner@gmail.com', 'owner', 'one')
-    channel_id = register_channel(extract_token(owner_info), 'channel1', True).json()
-    error = create_standup(extract_token(owner_info), 12312, -1000).status_code
+    owner_token = extract_token(owner_info)
+
+    register_channel(owner_token, 'channel1', True).json()
+    error = create_standup(owner_token, 12312, -1000).status_code
     
     assert error == 400
 
-def test_create_standup_unauthorized_member(clear, register_channel, register_user, create_standup):
+def test_create_standup_unauthorized_member(clear, register_channel, register_user, create_standup, extract_token):
     owner_info = register_user('owner@gmail.com', 'owner', 'one')
+    owner_token = extract_token(owner_info)
+
     user_info = register_user('user@gmail.com', 'user', 'one')
-    channel_id = register_channel(extract_token(owner_info), 'channel1', False).json()
+    channel_id = register_channel(owner_token, 'channel1', False).json()
     error = create_standup(extract_token(user_info), channel_id, 1000).status_code
 
     assert error == 403
 
-def test_create_priority_error(clear, register_channel, register_user, create_standup):
+def test_create_priority_error(clear, register_channel, register_user, create_standup, extract_token):
     owner_info = register_user('owner@gmail.com', 'owner', 'one')
+    owner_token = extract_token(owner_info)
+
     user_info = register_user('user@gmail.com', 'user', 'one')
-    channel_id = register_channel(extract_token(owner_info), 'channel1', False).json()
-    create_standup(extract_token(owner_info), channel_id, 1000).json()
+    channel_id = register_channel(owner_token, 'channel1', False).json()
+    create_standup(owner_token, channel_id, 1000).json()
     error = create_standup(extract_token(user_info), channel_id, -1000).status_code
 
     assert error == 400
 
-def test_create_priotiy_error(clear, register_channel, register_user, create_standup):
+def test_create_priotiy_error(clear, register_channel, register_user, create_standup, extract_token):
     owner_info = register_user('owner@gmail.com', 'owner', 'one')
-    channel_id1 = register_channel(extract_token(owner_info), 'channel1', False).json()
-    channel_id2 = register_channel(extract_token(owner_info), 'channel2', False).json()
-    standup_id1 = create_standup(extract_token(owner_info), channel_id1, 1000).json()
-    standup_id2 = create_standup(extract_token(owner_info), channel_id2, 1000).json()
+    owner_token = extract_token(owner_info)
+
+    channel_id1 = register_channel(owner_token, 'channel1', False).json()
+    channel_id2 = register_channel(owner_token, 'channel2', False).json()
+    standup_id1 = create_standup(owner_token, channel_id1, 1000).json()
+    standup_id2 = create_standup(owner_token, channel_id2, 1000).json()
+    now = datetime.now().timestamp()
 
     assert standup_id1['time_created'] == pytest.approx(now + 1000, rel=2)
     assert standup_id2['time_created'] == pytest.approx(now + 1000, rel=2)
