2531313a9ffbd89c10d72f45d93737cd103635f9
Cheng Li <cheng.li9@student.unsw.edu.au>
2021-11-07 19:12:30 +1100

    updated datastore for user_profile_upload_photo

--- src/data_store.py
+++ src/data_store.py
@@ -262,11 +262,6 @@ class Datastore:
     # handle_str
 
     def get_u_id_from_handle_str(self, handle_str):
-<<<<<<< HEAD
-        users = self.get_users_from_u_id_dict()
-        return [users[user]['u_id'] for user in users if users[user]['handle_str'] == handle_str][0]
-
-=======
         users = self.get_users_from_u_id_dict
         return [users[user]['u_id'] for user in users if users[user]['handle_str'] == handle_str][0]
 
@@ -280,7 +275,6 @@ class Datastore:
             
         return name
 
->>>>>>> c24ff9756deaa2f1ad54821f40af80dd331f8110
     # Check Methods ############################################################
 
     def is_token_invalid(self, token):
@@ -660,6 +654,12 @@ class Datastore:
 
         self.update_pickle()
 
+    def update_profile_img_url(self, auth_user_id, img_url):
+        user = self.get_users_from_u_id_dict().get(auth_user_id)
+        user['profile_img_url'] = img_url
+
+        self.update_pickle()
+
     def update_value(self, dict_key, key, value):
         self.__store[dict_key][key] = value
         self.update_pickle()
@@ -670,13 +670,10 @@ class Datastore:
         self.__store['message_count'] += 1
         self.update_pickle()
 
-<<<<<<< HEAD
-=======
     def decrease_message_count(self):
         self.__store['message_count'] -= 1
         self.update_pickle()
 
->>>>>>> c24ff9756deaa2f1ad54821f40af80dd331f8110
     def set(self, store):
         if not isinstance(store, dict):
             raise TypeError('store must be of type dictionary')
