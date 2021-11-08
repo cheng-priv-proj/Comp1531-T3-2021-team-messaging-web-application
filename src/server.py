92eb1591784793da64127a85c7bc579c3c51794a
Alexander Chen <alexander.l.chen@student.unsw.edu.au>
2021-11-09 03:29:27 +1100

    Update src/server.py

--- src/server.py
+++ src/server.py
@@ -1315,5 +1315,5 @@ def get_profile_pic():
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, quit_gracefully) # For coverage
-    APP.run(port=config.port, debug=True) # Do not edit this port
+    APP.run(port=config.port) # Do not edit this port
     
