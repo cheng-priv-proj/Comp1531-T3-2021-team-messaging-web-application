fb80970603ae219eece6dd72b4b6f80d789ab278
Aslyui <alex.ljchen520@gmail.com>
2021-11-08 23:37:55 +1100

    fixed pylint

--- tests/user_stats_v1_test.py
+++ tests/user_stats_v1_test.py
@@ -130,7 +130,6 @@ def test_user_stats_v1_joined_channels(clear_server, get_valid_token, channel_fa
 def test_user_stats_v1_joined_dms(clear_server, get_valid_token, dm_factory, send_message_dm_factory):
     user = get_valid_token
     token = user['token']
-    auth_id = user['auth_user_id']
     dm1 = dm_factory(token, [])
     dm2 = dm_factory(token, [])
     send_message_dm_factory(token, dm1, 'hello')
@@ -162,7 +161,6 @@ def test_user_stats_v1_joined_dms(clear_server, get_valid_token, dm_factory, sen
 def test_user_stats_v1_joined_channels_and_dms(clear_server, get_valid_token, channel_factory, send_message_dm_factory, send_message_channel_factory, dm_factory):
     user = get_valid_token
     token = user['token']
-    auth_id = user['auth_user_id']
 
     channel1 = channel_factory(token, 'channel1')
     channel_factory(token, 'channel2')
